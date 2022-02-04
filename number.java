4dfaf737bac62144a1af22a415f1d6a5e3f414cd
Murali8344771858 <muraliprawin@gmail.com>
2022-02-04 17:23:20 +0530

    hh

---
+++ number.java
@@ -0,0 +1,17 @@
+import java.io.*;
+class number
+{
+public static void main(String args[])
+{
+for(int i=1,i<=15,i++)
+{
+for(int j=0,j<=i,j++)
+{
+System.out.println("1");
+{
+System.out.println();
+}
+}
+}
+}
+} 
